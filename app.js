69e783d244280b770585a3fab1009def68cca5b1
jenyyy4 <anuskaroy0410@gmail.com>
2025-09-16 19:46:44 +0530

    debugging 2.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
-const User = require('./models/User');
+const User = require('./models/user');
 const Blog = require('./models/blog');
 const blogController = require('./controller/blogController');
 
